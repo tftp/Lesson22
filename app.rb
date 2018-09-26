3f204e90f1473acc364246077074e9bb9d28d4b7
tftp <goloborodko@inbox.ru>
2018-09-26 10:46:08 +0300

    remake target, add chating, contacts, zakaz

--- app.rb
+++ app.rb
@@ -6,3 +6,16 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/zakaz' do
+	erb :zakaz
+end
+
+get '/chating' do
+	erb :chating
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
