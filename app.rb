6494009d6507215589582ccfa0e50b8ef3be8961
tftp <goloborodko@inbox.ru>
2018-09-28 10:45:50 +0300

    adding new contacts and txt file message.txt

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :welcome		
 end
 
 get '/zakaz' do
@@ -20,14 +20,20 @@ get '/contacts' do
 end
 
 post '/zakaz' do
-	to_file :name=>params["username"], :adres=>params["adres"], :email_name=>params["email_name"]
+	to_file :file_name=>"./public/users.txt", :name=>params["username"], :adres=>params["adres"], :email_name=>params["email_name"]
 	erb "Спасибо за Ваш заказ, мы свяжемся с Вами в ближайшее время."
 end
 
+post '/contacts' do
+	to_file :file_name=>"./public/messages.txt", :message=>params["message"], :email_name=>params["email_name"]
+	erb "Сообщение отправлено. Спасибо."
+end
+
 def to_file hh
-	f = File.open("./public/users.txt", "a") 
+	f = File.open(hh[:file_name], "a") 
 	f.write(hh[:name]+"  ") if hh[:name]
-	f.write(hh[:adres]+"  ") if hh[:adres]
 	f.write(hh[:email_name]+" \n ") if hh[:email_name]
+	f.write(hh[:adres]+"\n\n") if hh[:adres]
+	f.write(hh[:message].strip+"\n\n") if hh[:message]
 	f.close
 end
