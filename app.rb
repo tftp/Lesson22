de47527ec6231d4a224e69622ba0ee4fd21c73a1
tftp <goloborodko@inbox.ru>
2018-11-19 16:51:43 +0300

    correction app.rb for activerecord

--- app.rb
+++ app.rb
@@ -10,26 +10,6 @@ set :database, "sqlite3:school.db"
 class Users < ActiveRecord::Base
 end
 
-def get_db 
-	@db = SQLite3::Database.new 'school.db'
-	@db.results_as_hash = true
-end
-
-before do
-	get_db
-end
-
-configure do
-	get_db
-	@db.execute 'create table if not exists 
-		"user" 
-		(	"id" integer primary key autoincrement, 
-			"username" text, 
-			"adres" text, 
-			"email" text
-		) '
-end
-
 
 get '/' do
 	erb :welcome		
@@ -58,7 +38,8 @@ post '/zakaz' do
 	if @error != ''
 		return erb :zakaz
 	end
-	@db.execute 'insert into user (username, adres, email) values (?, ?, ?)', [@username, @adres, @email_name]
+
+	Users.create :username=>@username, :adres=>@adres, :email=>@email_name
 
 	erb "Спасибо за Ваш заказ, мы свяжемся с Вами в ближайшее время."
 end
@@ -85,7 +66,7 @@ end
 
 get '/showusers' do
 	@row=''
-	@res_db = @db.execute 'select * from user'
+	@res_db = Users.all
 	erb :showusers
 end
 
