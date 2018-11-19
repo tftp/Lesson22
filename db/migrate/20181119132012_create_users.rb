47c387cc008cdc92513b4bdcf7a1e07882afb98a
tftp <goloborodko@inbox.ru>
2018-11-19 16:35:51 +0300

    corrections

--- db/migrate/20181119132012_create_users.rb
+++ db/migrate/20181119132012_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     	create_table :users do |t|
-		t.text :username
-		t.text :adres
-		t.text :email
+			t.text :username
+			t.text :adres
+			t.text :email
 
-		t.timestamps
+			t.timestamps
+		end
 
   end
 end
